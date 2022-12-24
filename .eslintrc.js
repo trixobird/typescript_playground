9ba29d3f283d28857e24ff0370f0a968d1b6fa58
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2022-12-24 12:50:43 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+  }
+}
