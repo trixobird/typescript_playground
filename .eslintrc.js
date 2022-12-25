bcb57ceae1712e97664812ab9838e63f77a31a9b
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2022-12-25 10:12:40 +0200

    Intrioduce config that holds tsconfig.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     es2021: true,
   },
   extends: ['standard-with-typescript', 'prettier'],
+  ignorePatterns: ["**/dist/**/*"],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
